2eaac835c407cf0d2925dd4c708fe9d63af92b3a
leonmslz <91141541+leonmslz@users.noreply.github.com>
2024-01-21 18:40:36 +0100

    Started simplifying Emacs Configuration

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -10,7 +10,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-			 '("melpa" . "https://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
@@ -31,138 +31,94 @@
 
 ;; Profile Emacs Startup
 (add-hook 'emacs-startup-hook
-		  (lambda ()
-			(message "*** Emacs loaded in %s seconds with %d garbage collections."
-					 (emacs-init-time "%.2f")
-					 gcs-done)))
+          (lambda ()
+            (message "*** Emacs loaded in %s seconds with %d garbage collections."
+                     (emacs-init-time "%.2f")
+                     gcs-done)))
 
-;; => Emacs Server / Deamon
+;; => Basic Options
 
-(use-package server
-  :ensure t
+;; Relative Line Numbers
+(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-width-start t)
+(setq display-line-numbers-major-tick 10)
+(global-display-line-numbers-mode t)
 
-  :config
-  (unless (server-running-p)
-	(server-start)))
+;; Use Spaces instead of Tabs
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
 
-;; => Options.el
-;; Plugin To Group Options Together And Maintain Consistency Across Emacs
-;; Regarding The Setting Of Options <https://github.com/leonmslz/options.el>
+;; Disable Line Wrapping
+(setq-default truncate-lines 1)
+(setq         truncate-partical-width-windows nil)
+;; If Enabled Behave Like Wrapped Lines Are Seperate
+(global-visual-line-mode t)
 
-(use-package options
-  :load-path "options.el/"
-  :ensure nil
+;; Hightlight Cursorline
+(global-hl-line-mode t)
 
-  :config
-  (progn
+;; Inhibit Startup Screen
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message "")
 
-	(op-c line-numbers
-		  :desc   "Enable Relative Line Numbers"
-		  :set    ((display-line-numbers-type        . relative)
-				   (display-line-numbers-width-start . t)
-				   (display-line-numbers-major-tick  . 10))
-		  :on     (global-display-line-numbers-mode)
-		  :hook   (:off (dired-mode-hook
-						 eshell-mode-hook
-						 term-mode-hook))
-		  :ensure t)
+;; Disable Unnecessary UI-Elements
+(menu-bar-mode   -1)
+(tool-bar-mode   -1)
+(scroll-bar-mode -1)
+(tooltip-mode    -1)
 
-	(op-c tabs
-		  :desc    "Set Tab Width To 4 Spaces."
-		  :set     ((:setdef tab-width . 4))
-		  :ensure  t)
+;; Change Default Scrolling Behaviour
+(setq scroll-conservatively 10000)
+(setq scroll-preserve-screen-position 'always)
+(setq mouse-wheel-progressive-speed nil)
+(setq scroll-step 1)
+(setq scroll-margin 4)
 
-	(op-c line-wrapping
-		  :desc    "Disable Line Wrapping Globally. If It Is Enabled Behave Like Wrapped Lines Are Seperate Lines."
-		  :set     ((:setdef truncate-lines . 1)
-					(truncate-partial-width-windows . nil))
-		  :on      (visual-line-mode)
-		  :ensure  t)
+;; Disable Autosaves And Backups
+(setq-default auto-save-mode 0)
+(setq-default make-backup-files nil)
 
-	(op-c hl-line
-		  :desc   "Highlight Cursor Line."
-		  :on     (global-hl-line-mode)
-		  :hook   (:off (term-mode-hook
-						 eshell-mode-hook))
-		  :ensure t)
+;; Balanced Windows <https://zck.org/balance-emacs-windows>
+(setq window-combination-resize t)
 
-	(op-c ui-elements
-		  :desc   "Disable Unnecessary UI-Elements And Inhibit Startup Screen"
-		  :set    ((inhibit-startup-screen . t)
-				   (initial-scratch-message . ""))
-		  :off    (menu-bar-mode
-				   tool-bar-mode
-				   scroll-bar-mode
-				   tooltip-mode)
-		  :ensure t)
+;; Automaticly Follow Symbolic Links
+(setq vc-follow-symlinks t)
 
-	(op-c scrolling-behaviour
-		  :desc  "Change Default Scrolling Behaviour"
-		  :set   ((scroll-conservatively . 10000)
-				  (scroll-preserve-screen-position . always)
-				  (mouse-wheel-progressive-speed . nil)
-				  (scroll-step . 1)
-				  (scroll-margin . 4))
-		  :hook   (:off (term-mode-hook
-						 eshell-mode-hook))
-		  :ensure t)
+;; Prettify Symbols
+(global-prettify-symbols-mode 1)
 
-	(op-c no-backups
-		  :desc   "Disable Autosaves And Backups"
-		  :set    ((auto-save-mode . 0)
-				   (make-backup-files . nil))
-		  :ensure t)
+;; Enable Deletion Of Selected Text
+(delete-selection-mode 1)
 
-	(op-c balanced-windows
-		  :desc   "Balanced Windows <https://zck.org/balance-emacs-windows>"
-		  :set    ((window-combination-resize . t))
-		  :ensure t)
+;; Enable Autopairs
+(electric-pair-mode 1)
 
-	(op-c symbolic-links
-		  :desc   "Automaticly Follow Symbolic Links"
-		  :set    ((vc-follow-symlinks . t))
-		  :ensure t)
+;; Delete Trailling Whitespace On Save
+(add-hook 'before-save-hook 'whitespace-cleanup)
 
-	(setq disabled-command-function nil)
+;; Wrap Around In Minibuffer When Selecting Completion Options
+(setq completion-auto-wrap t)
 
-	;; Enable Deletion Of Selected Text
-	(delete-selection-mode 1)
-
-	;; Enable Autopairs
-	(electric-pair-mode 1)
-
-	;; Delete Trailling Whitespace On Save
-	(add-hook 'before-save-hook 'whitespace-cleanup)
-
-	;; Wrap Around In Minibuffer When Selecting Completion Options
-	(setq completion-auto-wrap t)
-
-	;; Make Background Transparent
-	(add-to-list 'default-frame-alist '(alpha-background . 85))
-
-	;; Set Font-Face
-	(set-frame-font "JetBrains Mono 10" nil t)
-
-	))
+;; Make Background Transparent
+(add-to-list 'default-frame-alist '(alpha-background . 85))
 
 ;; :==:> Theming / Appereance
 
 ;; => Colorscheming
+(use-package ef-themes
+  :ensure t
+  :config
+  (load-theme 'ef-spring t))
 
-;; (use-package ef-themes
-;;   :ensure t
+;; => Font-Face
+(set-face-attribute 'default
+                    nil
+                    :family "Iosevka"
+                    :height 105
+                    :weight 'semilight)
 
-;;   :config
-;;   (load-theme 'ef-spring t))
-
-(add-to-list 'custom-theme-load-path "/home/leon/.config/emacs/visionary-theme.el/")
-(load-theme 'visionary-green t)
-
+;; Custom Faces For Org-Mode
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(org-level-1 ((t (:inherit outline-1 :height 1.6))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
@@ -183,22 +139,22 @@
   Last Updated: 25.08.2023."
 
   `(let (start-in-buffer
-		 end-in-buffer)
+         end-in-buffer)
 
-	 (setq start-in-buffer (buffer-name))
+     (setq start-in-buffer (buffer-name))
 
-	 (,next-or-prev)
+     (,next-or-prev)
 
-	 (let ((i 0))
-	   (while (< i 20)
-		 (if (string-equal "*" (substring (buffer-name) 0 1))
-			 (progn (,next-or-prev)
-					(setq i (+ i 1)))
-		   (setq i 100
-				 end-in-buffer (buffer-name)))))
+     (let ((i 0))
+       (while (< i 20)
+         (if (string-equal "*" (substring (buffer-name) 0 1))
+             (progn (,next-or-prev)
+                    (setq i (+ i 1)))
+           (setq i 100
+                 end-in-buffer (buffer-name)))))
 
-	 (when (eq start-in-buffer end-in-buffer)
-	   (error "There Is Only One User Buffer Available!"))))
+     (when (eq start-in-buffer end-in-buffer)
+       (error "There Is Only One User Buffer Available!"))))
 
 (defun cef-next-user-buffer ()
 
@@ -235,20 +191,20 @@
    Last Updated: 25.08.2023."
 
   `(let (other-window-p
-		 buffer-this-buffer)
+         buffer-this-buffer)
 
-	 (setq other-window-p     (windmove-find-other-window ',direction)
-		   buffer-this-buffer (window-buffer (selected-window)))
+     (setq other-window-p     (windmove-find-other-window ',direction)
+           buffer-this-buffer (window-buffer (selected-window)))
 
-	 (when (or (null other-window-p)
-			   (and (eq ',direction 'down)
-					(string-match "^ \\*Minibuf" (buffer-name (window-buffer other-win)))))
-	   (error "Unable To Swap Buffers."))
+     (when (or (null other-window-p)
+               (and (eq ',direction 'down)
+                    (string-match "^ \\*Minibuf" (buffer-name (window-buffer other-win)))))
+       (error "Unable To Swap Buffers."))
 
-	 (set-window-buffer (selected-window) (window-buffer other-window-p))
+     (set-window-buffer (selected-window) (window-buffer other-window-p))
 
-	 (set-window-buffer other-window-p buffer-this-buffer)
-	 (select-window other-window-p)))
+     (set-window-buffer other-window-p buffer-this-buffer)
+     (select-window other-window-p)))
 
 (defun cef-move-buffer-up ()
 
@@ -310,8 +266,6 @@
 
   (kill-buffer (buffer-name)))
 
-(global-set-key (kbd "C-x k") 'cef-kill-current-buffer)
-
 ;; :==:> Custom Functions
 
 ;; From <https://emacs.stackexchange.com/questions/7148/get-all-regexp-matches-in-buffer-as-a-list>
@@ -324,14 +278,14 @@
 
   (save-match-data
 
-	(let ((pos 0)
-		  matches)
+    (let ((pos 0)
+          matches)
 
-	  (while (string-match regexp string pos)
-		(push (match-string 0 string) matches)
-		(setq pos (match-end 0)))
+      (while (string-match regexp string pos)
+        (push (match-string 0 string) matches)
+        (setq pos (match-end 0)))
 
-	  matches)))
+      matches)))
 
 (defun cef-grap-region-or-line ()
 
@@ -343,13 +297,13 @@
 
   (if mark-active
 
-	  (buffer-substring-no-properties
-	   (region-beginning)
-	   (region-end))
+      (buffer-substring-no-properties
+       (region-beginning)
+       (region-end))
 
-	(buffer-substring-no-properties
-	 (point-at-bol)
-	 (point-at-eol))))
+    (buffer-substring-no-properties
+     (point-at-bol)
+     (point-at-eol))))
 
 (defun cef-extract-urls-from-string (string)
 
@@ -360,8 +314,8 @@
 
   (let (url-regexp)
 
-	(setq url-regexp
-		  "\\b\\(https?://\\|www\\.\\)[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]")
+    (setq url-regexp
+          "\\b\\(https?://\\|www\\.\\)[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]")
 
   (cef-multiple-matches url-regexp string)))
 
@@ -380,10 +334,10 @@
    Last Updated: 26.08.2023."
 
   `(dolist (url
-		   (cef-extract-urls-from-string (cef-grap-region-or-line)))
+           (cef-extract-urls-from-string (cef-grap-region-or-line)))
 
-	(shell-command
-	 (format "%s \"%s\"" ,browser-cmd url))))
+    (shell-command
+     (format "%s \"%s\"" ,browser-cmd url))))
 
 (defun cef-open-url-in-current-browser-session ()
 
@@ -409,9 +363,6 @@
   (cef-open-url-with
    (concat cef-browser " -new-window")))
 
-(global-set-key (kbd "C-c C-c o i b") 'cef-open-url-in-current-browser-session)
-(global-set-key (kbd "C-c C-c o i w") 'cef-open-url-in-new-browser-session)
-
 ;; => Copy URL
 
 (defun cef-copy-url ()
@@ -424,10 +375,8 @@
   (interactive)
 
   (dolist (url
-		   (cef-extract-urls-from-string (cef-grap-region-or-line)))
-	(kill-new url)))
-
-(global-set-key (kbd "C-c C-c c u") 'cef-copy-url)
+           (cef-extract-urls-from-string (cef-grap-region-or-line)))
+    (kill-new url)))
 
 ;; => Selecting Text
 
@@ -435,20 +384,15 @@
   ""
   (interactive)
   (if mark-active
-	  (keyboard-escape-quit)
-	(set-mark (point))))
+      (keyboard-escape-quit)
+    (set-mark (point))))
 
 (defun toggle-rectangle-selection ()
   ""
   (interactive)
   (if mark-active
-	  (keyboard-escape-quit)
-	(rectangle-mark-mode)))
-
-(global-set-key (kbd "C-SPC") 'toggle-selection)
-
-(global-set-key (kbd "C-c p") 'mark-paragraph)
-(global-set-key (kbd "C-c r") 'toggle-rectangle-selection)
+      (keyboard-escape-quit)
+    (rectangle-mark-mode)))
 
 ;; => Open A Terminal In The Current Pane
 
@@ -469,10 +413,8 @@
   ;; Automaticly Kill Buffer When Exiting The Shell
   ;; <https://stackoverflow.com/questions/12624815/how-to-automatically-kill-buffer-on-terminal-process-exit-in-emacs>
   (defadvice term-handle-exit
-	  (after term-kill-buffer-on-exit activate)
-	(kill-buffer)))
-
-(global-set-key (kbd "C-c C-c o t") 'cef-open-terminal)
+      (after term-kill-buffer-on-exit activate)
+    (kill-buffer)))
 
 ;; => Inserting Current Date
 
@@ -487,101 +429,73 @@
   (insert
    (format-time-string "%d.%m.%Y")))
 
-(global-set-key (kbd "C-c C-c i d") 'cef-insert-todays-date)
-
 ;; => Moving Lines
 ;; Copied From <https://www.emacswiki.org/emacs/MoveLine>
 
 (defmacro cef-save-column (&rest body)
   ""
   `(let ((column (current-column)))
-	 (unwind-protect
-		 (progn ,@body)
-	   (move-to-column column))))
+     (unwind-protect
+         (progn ,@body)
+       (move-to-column column))))
 (put 'cef-save-column 'lisp-indent-function 0)
 
 (defun cef-move-line-up ()
   ""
   (interactive)
   (cef-save-column
-	(transpose-lines 1)
-	(forward-line -2)))
-(global-set-key (kbd "M-p") 'cef-move-line-up)
+    (transpose-lines 1)
+    (forward-line -2)))
 
 (defun cef-move-line-down ()
   ""
   (interactive)
   (cef-save-column
-	(forward-line 1)
-	(transpose-lines 1)
-	(forward-line -1)))
-(global-set-key (kbd "M-n") 'cef-move-line-down)
-
-;; => Testing Certain Functions
-
-;; Open Dired In A Side Split
-(defun cef-dired-side-window ()
-  "Open Dired In "
-  (interactive)
-  (display-buffer-in-side-window
-   (dired-noselect default-directory) `((side . left)
-										(slot . 0)
-										(window-width . 0.2)))
-  (other-window))
-(global-set-key (kbd "C-c w d") 'cef-dired-side-window)
+    (forward-line 1)
+    (transpose-lines 1)
+    (forward-line -1)))
 
 ;; :==:> Keybindings
 
-;; => General.el
-;; More Convenient Key Definitions In Emacs <https://github.com/noctuid/general.el>
+;; Custom Keybinding Definitions
 
-(use-package general
-  :ensure t
+(global-set-key (kbd "C-c w s")			'split-window-vertically)
+(global-set-key (kbd "C-c w v")			'split-window-horizontally)
 
-  :init
-  (require 'general)
+(global-set-key (kbd "C-c w k")			'delete-window)
+(global-set-key (kbd "C-c w z")			'delete-other-windows)
 
-  ;; Window And Buffer Managment Related Keybindings
-  (general-define-key
-   :prefix "C-c"
+(global-set-key (kbd "C-c o")			'other-window)
 
-   ;; Switching User Buffers
-   "w c p" 'cef-prev-user-buffer       ;; Switch To Previous User Buffer
-   "w c n" 'cef-next-user-buffer       ;; Switch To Next User Buffer
+(global-set-key (kbd "C-c w f")			'cef-move-buffer-right)
+(global-set-key (kbd "C-c w b")			'cef-move-buffer-left)
+(global-set-key (kbd "C-c w n")			'cef-move-buffer-down)
+(global-set-key (kbd "C-c w p")			'cef-move-buffer-up)
 
-   ;; Splitting Windows
-   "w s" 'split-window-vertically      ;; Split The Current Window Horizontally
-   "w v" 'split-window-horizontally    ;; Split The Current Window Vertically
+(global-set-key (kbd "C-.")				'repeat)
 
-   ;; Close Windows
-   "w k" 'delete-window                ;; Close The Current Window
-   "w z" 'delete-other-windows         ;; Close Every Window Except The Current Window
+(global-set-key (kbd "C-,")				'comment-line)
 
-   ;; Moving Between Splits
-   "o"   'other-window                 ;; Switch To The Next Split
-   "C-o" 'other-window                 ;; Alias For "C-c o"
-   "w f" 'windmove-right               ;; Move Right To The Next Split
-   "w b" 'windmove-left                ;; Move Left To The Next Split
-   "w n" 'windmove-down                ;; Move Down To The Next Split
-   "w p" 'windmove-up                  ;; Move Up To The Next Split
+(global-set-key (kbd "C--")				'undo-redo)
+(global-set-key (kbd "C-_")				'undo)
 
-   ;; Moving Buffers Around
-   "w m f" 'cef-move-buffer-right      ;; Move The Current Buffer To The Right
-   "w m b" 'cef-move-buffer-left       ;; Move The Current Buffer To The Left
-   "w m n" 'cef-move-buffer-down       ;; Move The Current Buffer Down
-   "w m p" 'cef-move-buffer-up         ;; Move The Current Buffer Up
-   )
+(global-set-key (kbd "M-p")				'cef-move-line-up)
+(global-set-key (kbd "M-n")				'cef-move-line-down)
 
-  ;; Miscellaneous Keybindings
-  (general-define-key
+(global-set-key (kbd "C-c C-c i d")		'cef-insert-todays-date)
+(global-set-key (kbd "C-c C-c o t")		'cef-open-terminal)
 
-   "C-." 'repeat                       ;; Repeat Last Command
+(global-set-key (kbd "C-SPC")			'toggle-selection)
 
-   "C-," 'comment-line                 ;; Toggle Comment On Current Line / Region
+(global-set-key (kbd "C-c p")			'mark-paragraph)
+(global-set-key (kbd "C-c r")			'toggle-rectangle-selection)
 
-   "C--" 'undo-redo                    ;; Redo Last Change
-   "C-_" 'undo)                        ;; Undo Last Change
-)
+(global-set-key (kbd "C-c C-c c u")		'cef-copy-url)
+
+(global-set-key (kbd "C-c C-c o i b")	'cef-open-url-in-current-browser-session)
+(global-set-key (kbd "C-c C-c o i w")	'cef-open-url-in-new-browser-session)
+
+(global-set-key (kbd "C-x k")			'cef-kill-current-buffer)
 
 ;; => Fuzzy Finding
 
@@ -608,11 +522,11 @@
 (use-package consult
   :ensure t
   :bind (("C-u"     . consult-buffer)
-		 ("C-x b"   . consult-buffer)
-		 ("C-c m b" . consult-buffer)
-		 ("C-c m t" . consult-theme)
-		 ("C-c m f" . consult-line)
-		 ("C-c m y" . consult-yank-pop)))
+         ("C-x b"   . consult-buffer)
+         ("C-c m b" . consult-buffer)
+         ("C-c m t" . consult-theme)
+         ("C-c m f" . consult-line)
+         ("C-c m y" . consult-yank-pop)))
 
 ;; => Language Support
 
@@ -670,10 +584,3 @@
   :ensure t
   :config
   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("5a3b5c6e95d124fcc23892b97b0a6743376494368fe9566ab8825f637777e693" "b14446ae9fbe8268f6e06f070b62b96b44bb9ad4d10d08848b8eb1a74e6085bc" "6ab94fa8d4bcf530b72674fc8e947db23c044f5e08f34a06ea8eed109c788397" "405e8bbef0f6d8ecd19022ee90fb1234fed049f4b6ed4913e12658ed2fd6dc89" "9f6ba7637b4c3ceb5c697d8dcba8c0e84b506ef11afb439c78b1a8812753116a" "e8ff4936109610b67af0b3d83f95e05224e89a3cb7ada1b4c9c617f3c59319fe" "917599da0c9d9637a95d76a17cf6ecd3e054a4c0236e6d32393a5476b3b6a871" "e116116d2d54141cada3cb72d49d0b5a9aae594a6af86efa2d374b9c793391f7" "73bffcbedeb01f4523d8c9da2715f307aa8627c4a8725037f20a17bc28e1b2d3" "34e207bc4960f5bd23d97c0a8759fec8a97a473b70f9c9c6a5aa2ec9d768d218" "99aef65cee60667ae9d4ff45fe02467aea07b2f31ea618954aa29b392de95103" "4a52fbeb6eb8413b19cd48155b4010670c18706f61765971ba528312db3e811c" default)))
