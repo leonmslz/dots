6f870f8101342d7e4b66e4699ecd2ca0c1040787
leonmslz <91141541+leonmslz@users.noreply.github.com>
2024-06-17 21:31:52 +0200

    Grub Gruvbox Theming

--- system/boot/grub.nix
+++ system/boot/grub.nix
@@ -1,6 +1,14 @@
 # grub.nix - Configuration File For Grub-Bootloader
-{ ... }:
+{ pkgs, ... }:
 
+let
+  grub-gruvbox-theme = pkgs.fetchFromGitHub {
+    owner  = "AllJavi";
+    repo   = "tartarus-grub";
+    rev    = "b116360a2a0991062a4d728cb005dfd309fbb82a";
+    sha256 = "0igy1aqp3v2v8gqqlk0p5i78g9l3xz19fh3aigafbk7k3p8ypz7w";
+  } + "/tartarus";
+in
 {
   # --- Grub ---
 
@@ -9,6 +17,7 @@
   # Enable Grub
   boot.loader.grub = {
     enable      = true;
+    theme       = grub-gruvbox-theme;
     useOSProber = true;
     devices     = [ "nodev" ];
     efiSupport  = true;
