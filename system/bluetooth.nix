07079cab26a04d971abf93c2cf4bf5103b59fe35
leonmslz <91141541+leonmslz@users.noreply.github.com>
2024-06-26 17:12:35 +0200

    AirPods :D

--- system/bluetooth.nix
+++ system/bluetooth.nix
@@ -2,8 +2,18 @@
 { config, ... }:
 
 {
+  # Enable Bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
+  # AirPods
+  # Comment this in the first time you want to connect to AirPods.
+  # In order to connect, you have to press the button on the back
+  # of the AirPods case.
+  # `breder` is only needed for the initial connection of the AirPods.
+  # Afterwards the mode can be relaxed to `dual` (the default) again.
+  hardware.bluetooth.settings = { General = { ControllerMode = "bredr"; }; };
+
+  # Use Blueman Bluetooth-Manager
   services.blueman.enable = true;
 }
