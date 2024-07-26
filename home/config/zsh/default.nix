5d7e326fbbfd97a5a7a3d56411cd9a04c35ff46c
leonmslz <91141541+leonmslz@users.noreply.github.com>
2024-07-26 11:03:09 +0200

    Split Up Zsh Configuration :D

--- home/config/zsh/default.nix
+++ home/config/zsh/default.nix
@@ -1,7 +1,16 @@
+# default.nix (home/config/zsh) - Setup Symlinks For Zsh Configuration
 { config, ... }:
 
 {
-  home.file.".zshrc" = {
-    source = config.lib.file.mkOutOfStoreSymlink /home/leon/NixOS-System/home/config/zsh/rc.zsh;
+  home = {
+    file = {
+      ".config/zsh" = {
+        source = config.lib.file.mkOutOfStoreSymlink /home/leon/NixOS-System/home/config/zsh/.;
+        recursive = true;
+      };
+      ".zshrc" = {
+        source = config.lib.file.mkOutOfStoreSymlink /home/leon/.config/zsh/rc.zsh;
+      };
+    };
   };
 }
